e0273681bb9e13c0e24e1a907eba6fbad8b72d99
Сергей <nikitos841@yandex.ru>
2025-03-13 12:21:47 +0400

    add exceptions.py

--- src/exceptions.py
+++ src/exceptions.py
@@ -1,22 +1,40 @@
-from sqlalchemy.exc import SQLAlchemyError
-from fastapi import HTTPException, Request, status
+from logging import Logger, getLogger
+from typing import Any
+
+from fastapi import HTTPException, Request
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 
+logger: Logger = getLogger(__name__)
 
-async def custom_http_exception_handler(request: Request, exc: HTTPException) -> JSONResponse:
+
+async def custom_http_exception_handler(
+    request: Request, exc: HTTPException
+) -> JSONResponse:
+    logger.error("Ошибка http: %s", exc)
     return JSONResponse(
         status_code=exc.status_code,
-        content={"error": str(exc)},
+        content={"error http": str(exc)},
     )
 
-async def custom_request_validation_exception_handler(request: Request, exc: RequestValidationError) -> JSONResponse:
+
+async def custom_request_validation_exception_handler(
+    request: Request, exc: RequestValidationError
+) -> JSONResponse:
     return JSONResponse(
         status_code=422,
-        content={"message": "Собственный проверяльщик ошибок валидации", "errors": exc.errors()},
+        content={
+            "message": "Собственный проверяльщик ошибок валидации",
+            "errors": exc.errors(),
+        },
     )
 
-UserNotFoundException = HTTPException(
-    status_code=status.HTTP_404_NOT_FOUND,
-    detail='Пользователь не найден'
-)
+def valid_integer(value: Any) -> bool:
+    if not isinstance(value, int):
+        raise TypeError("Значение должно быть целым числом")
+    return True
+
+def valid_string(value: Any) -> bool:
+    if not isinstance(value, str):
+        raise TypeError("Значение должно быть строкой")
+    return True
