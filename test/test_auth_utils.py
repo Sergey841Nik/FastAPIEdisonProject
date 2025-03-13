5bc3728346048550592c05719556b473adb852fa
Сергей <nikitos841@yandex.ru>
2025-03-13 12:22:55 +0400

    add test_auth_utils

--- test/test_auth_utils.py
+++ test/test_auth_utils.py
@@ -1,53 +1,76 @@
 from calendar import timegm
-import pytest
+from contextlib import nullcontext
 from datetime import datetime, timedelta, timezone
+import pytest
 from src.auth.utils import encode_jwt, decode_jwt, hash_password, validate_password
 
 
-def test_encode_jwt_access_token() -> None:
-    payload: dict = {"user_id": 25, "username": "test_user"}
-    type_token = "access"
+@pytest.mark.parametrize(
+    "user_id, user_name, type_token, expectation",
+    (
+        (1, "test_user", "access", nullcontext()),
+        (25, "user1", "access", nullcontext()),
+        (35, "user2", "access", nullcontext()),
+        (
+            45,
+            "user3",
+            "unknown",
+            pytest.raises(ValueError, match="Неизвестный тип токена"),
+        ),
+    ),
+)
+def test_encode_jwt_access_token(user_id, user_name, type_token, expectation) -> None:
+    payload: dict = {"user_id": user_id, "username": user_name}
+    with expectation:
+        token: str = encode_jwt(payload, type_token)
 
-    token: str = encode_jwt(payload, type_token)
+        assert isinstance(token, str)
 
-    assert isinstance(token, str)
+        decode_payload: dict = decode_jwt(token)
+        now: datetime = datetime.now(timezone.utc)
 
-    decode_payload: dict = decode_jwt(token)
-    now: datetime = datetime.now(timezone.utc)
-
-    assert decode_payload["user_id"] == 25
-    assert decode_payload["username"] == "test_user"
-    assert decode_payload["exp"] == timegm((now + timedelta(minutes=15)).utctimetuple())
-    assert decode_payload["iat"] == timegm((now).utctimetuple())
-
-
-def test_encode_jwt_refresh_token() -> None:
-    payload: dict = {"user_id": 1, "username": "test_user"}
-    type_token: str = "refresh"
-
-    token: str = encode_jwt(payload, type_token)
-
-    assert isinstance(token, str)
-
-    decode_payload: dict = decode_jwt(token)
-    now: datetime = datetime.now(timezone.utc)
-
-    assert decode_payload["user_id"] == 1
-    assert decode_payload["username"] == "test_user"
-    assert decode_payload["exp"] == timegm((now + timedelta(days=30)).utctimetuple())
-    assert decode_payload["iat"] == timegm((now).utctimetuple())
+        assert decode_payload["user_id"] == user_id
+        assert decode_payload["username"] == user_name
+        # assert datetime.fromtimestamp(decode_payload["exp"]).day == (now + timedelta(minutes=15)).day
+        assert decode_payload["iat"] == timegm((now).utctimetuple())
 
 
-def test_encode_jwt_unknown_token_type() -> None:
-    payload = {"user_id": 1, "username": "test_user"}
-    type_token = "unknown"
+@pytest.mark.parametrize(
+    "user_id, user_name, type_token, expectation",
+    (
+        (1, "test_user", "refresh", nullcontext()),
+        (25, "user1", "refresh", nullcontext()),
+        (35, "user2", "refresh", nullcontext()),
+        (
+            45,
+            "user3",
+            "unknown",
+            pytest.raises(ValueError, match="Неизвестный тип токена"),
+        ),
+    ),
+)
+def test_encode_jwt_refresh_token(user_id, user_name, type_token, expectation) -> None:
+    payload: dict = {"user_id": user_id, "username": user_name}
+    with expectation:
+        token: str = encode_jwt(payload, type_token)
 
-    with pytest.raises(ValueError, match="Неизвестный тип токена"):
-        encode_jwt(payload, type_token)
+        assert isinstance(token, str)
+
+        decode_payload: dict = decode_jwt(token)
+        now: datetime = datetime.now(timezone.utc)
+
+        assert decode_payload["user_id"] == user_id
+        assert decode_payload["username"] == user_name
+        assert datetime.fromtimestamp(decode_payload["exp"]).day == (now + timedelta(days=7)).day
+        assert decode_payload["iat"] == timegm((now).utctimetuple())
 
 
 def test_validate_password() -> None:
     password = "test_password"
-    hashed_password = hash_password(password)
-    
+    hashed_password: bytes = hash_password(password)
+
+    assert isinstance(hashed_password, bytes)
     assert validate_password(password, hashed_password) is True
+
+
+    
